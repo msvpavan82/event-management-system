cc9acb5a3ad0ef48d426717cc02cb68f82955a11
msvpavan82 <msvpavan82@gmail.com>
2025-02-22 15:13:59 +0530

    defined the db schema for user and bookinngs table

---
+++ src/main/java/com/example/eventmanagementsystem/model/Booking.java
@@ -0,0 +1,22 @@
+package com.example.eventmanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    private int id;
+    private int eventId;
+    @JoinColumn(name = "person_id")
+    @ManyToOne
+    private Person person;
+}
---
+++ src/main/java/com/example/eventmanagementsystem/model/Person.java
@@ -0,0 +1,26 @@
+package com.example.eventmanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int age;
+    private long phone;
+    @OneToMany(mappedBy = "person")
+    private List<Booking> bookings;
+}
